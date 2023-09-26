0b667f7579a221bdb568717b438dceaefd1a0cd6
Parham Abdollahzadeh <nokashiyt@gmail.com>
2023-09-26 18:26:37 +0330

    AddUserModel modify

--- Core/Entity/AddUserModel/AddUserModel.cs
+++ Core/Entity/AddUserModel/AddUserModel.cs
@@ -5,11 +5,9 @@ namespace uni_project.Core.Entity.AddUserModel
     public class AddUserModel
     {
         [Required]
-        [StringLength(20)]
         public string FirstName { get; set; }
 
         [Required]
-        [StringLength(40)]
         public string LastName { get; set; }
 
         [Required]
@@ -21,17 +19,13 @@ namespace uni_project.Core.Entity.AddUserModel
         public string NationalCode { get; set; }
 
         [Required]
-        [StringLength(40)]
         public string FieldofStudy { get; set; }
 
-        [StringLength(200)]
         public string LastDocument { get; set; }
 
         [Required]
-        [StringLength(20)]
         public string Job { get; set; }
 
-        [StringLength(300)]
         public string JobAddress { get; set; }
 
         public AddUserModel(
